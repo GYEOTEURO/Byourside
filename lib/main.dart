2ec308c042a775890cd75612315a04caf8869673
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-09-10 18:07:09 +0900

    Chore: 상단 앱 바 title 이름 수정

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'temperature post page'),
+      home: const MyHomePage(title: 'Temperature Post Page'),
     );
   }
 }
