e8191013cb2b01731a04729ac9626cdf064c0dbf
qlxql <qlxqlrt2012@gmail.com>
2022-09-14 10:31:15 +0900

    Design : insert buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,37 @@
+import 'package:byourside/screen/login_screen.dart';
 import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(const MyApp());
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  bool login = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Post page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
+        title: '곁',
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+        ),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.asset("imgs/hug 1.png", width: 50,),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.asset("imgs/곁.png", width: 120,),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "아이디"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "비밀번호"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                      width: double.infinity,
-                      margin: EdgeInsets.only(top: 24),
-                      child: ElevatedButton(onPressed: () {}, child: Text("로그인"))),
-                ),
-                Padding(padding: const EdgeInsets.all(8.0),
-                child: TextButton(onPressed: () {  }, child: null,
-                  
-                ))
+                LoginScreen(),
+                LoginScreen(),
+                LoginScreen(),
+                LoginScreen(),
               ],
             ),
+            // bottomNavigationBar: Bottom(),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        )
     );
   }
 }
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  // final Login login;
+  // LoginScreen({required this.login});
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class Login {
+  String id = "";
+  String password = "";
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool login = false;
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Post page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.asset("images/hug 1.png", width: 50,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Image.asset("images/곁.png", width: 120,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "아이디"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "비밀번호"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(top: 24),
+                      child: ElevatedButton(onPressed: () {}, child: Text("로그인"))),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextButton(onPressed: () { }, child: Text("자동로그인"),),
+                      TextButton(onPressed: () { }, child: Text("|   회원가입"),),
+                      TextButton(onPressed: () { }, child: Text("|   아이디/비밀번호 찾기"),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: InkWell(
+                              radius: 100,
+                              onTap: () {
+                                ScaffoldMessenger.of(context).clearSnackBars();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text('kakao'),
+                                  duration: Duration(milliseconds: 1500),
+                                  ),
+                                );
+                              },
+                              child: Ink.image(
+                                  fit: BoxFit.cover,
+                                  image: const NetworkImage(
+                                      'https://www.kindacode.com/wp-content/uploads/2022/07/bottle.jpeg'),
+                            ),
+                          )
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: InkWell(
+                                radius: 100,
+                                onTap: () {
+                                  ScaffoldMessenger.of(context).clearSnackBars();
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text('kakao'),
+                                      duration: Duration(milliseconds: 1500),
+                                    ),
+                                  );
+                                },
+                                child: Ink.image(
+                                  fit: BoxFit.cover,
+                                  image: const NetworkImage(
+                                      'https://www.kindacode.com/wp-content/uploads/2022/07/bottle.jpeg'),
+                                ),
+                              )
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: InkWell(
+                                radius: 100,
+                                onTap: () {
+                                  ScaffoldMessenger.of(context).clearSnackBars();
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text('kakao'),
+                                      duration: Duration(milliseconds: 1500),
+                                    ),
+                                  );
+                                },
+                                child: Ink.image(
+                                  fit: BoxFit.cover,
+                                  image: const NetworkImage(
+                                      'https://www.kindacode.com/wp-content/uploads/2022/07/bottle.jpeg'),
+                                ),
+                              )
+                          ),
+                        )
+                      ]
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
