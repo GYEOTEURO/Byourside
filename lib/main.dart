0a0a9181b7e79408a3d8f090e6553a30e17091dd
Shin-MG <alsruddl0407@kookmin.ac.kr>
2022-09-17 01:26:25 +0900

    Feat: Bottom Navigation Bar 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:byourside/screen/bottomNavigationBar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -5,7 +6,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'Byourside';
+  static const String _title = '곁';
 
   @override
   Widget build(BuildContext context) {
@@ -24,69 +25,10 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Ondo',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Nanum',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Chat',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: MyPage',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) { // 탭을 클릭했을떄 지정한 페이지로 이동
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 158, 233, 197),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Color.fromARGB(255, 158, 233, 197),
-        unselectedItemColor: Color.fromARGB(255, 255, 255, 255),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.groups_rounded),
-            label: '마음온도',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.volunteer_activism),
-            label: '마음나눔',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: '채팅',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: '마이페이지',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 0, 0, 0),
-        onTap: _onItemTapped,
-      ),
+    return const Scaffold(
+      bottomNavigationBar: BottomNavBar(),
     );
   }
 }
