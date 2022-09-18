2d1d2307ac54ee02de35e65bebc612cce720a36b
qlxql <qlxqlrt2012@gmail.com>
2022-09-18 18:53:23 +0900

    Refactor : divide dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:byourside/screen/login_screen.dart';
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
+
+const primaryColor = Color(0xFF045558);
 
 class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
@@ -9,29 +12,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late TabController controller;
   bool login = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: '곁',
-        theme: ThemeData(
-            brightness: Brightness.dark,
-            primaryColor: Colors.black,
-        ),
-        home: DefaultTabController(
-          length: 4,
-          child: Scaffold(
-            body: TabBarView(
-              physics: NeverScrollableScrollPhysics(),
-              children: [
-                LoginScreen(),
-                LoginScreen(),
-                LoginScreen(),
-                LoginScreen(),
-              ],
-            ),
-            // bottomNavigationBar: Bottom(),
-          ),
-        )
+      title: '곁',
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => LoginScreen(primaryColor: primaryColor),
+        //"/home": (context) => HomePage(),
+      },
     );
   }
 }
---
+++ lib/widget/custom_text_form_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../size.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String text;
+
+  const CustomTextFormField(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(text),
+        SizedBox(height: small_gap),
+        TextFormField(
+          validator: (value) => value!.isEmpty
+              ? "Please enter some text"
+              : null, // 1. 값이 없으면 Please enter some text 경고 화면 표시
+          obscureText:
+              // 2. 해당 TextFormField가 비밀번호 입력 양식이면 **** 처리 해주기
+              text == "Password" ? true : false,
+          decoration: InputDecoration(
+            hintText: "Enter $text",
+            enabledBorder: OutlineInputBorder(
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
