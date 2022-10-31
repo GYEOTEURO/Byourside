03e9cf26107ab7f5b97fbf6864fbbbb67dbb3eb3
HaEun <qlxqlrt2012@gmail.com>
2022-10-31 23:38:21 +0900

    Feat : delete anonymous login

--- lib/widget/auth.dart
+++ lib/widget/auth.dart
@@ -14,16 +14,16 @@ class AuthService {
     return _auth.authStateChanges().map(_firebaseUser);
   }
 
-
-  Future signInAnonymous() async {
-    try {
-      UserCredential userCredential = await _auth.signInAnonymously();
-      User? user = userCredential.user;
-      return _firebaseUser(user);
-    } catch (e) {
-      return FirebaseUser(code: e.toString(), uid: null);
-    }
-  }
+  //
+  // Future signInAnonymous() async {
+  //   try {
+  //     UserCredential userCredential = await _auth.signInAnonymously();
+  //     User? user = userCredential.user;
+  //     return _firebaseUser(user);
+  //   } catch (e) {
+  //     return FirebaseUser(code: e.toString(), uid: null);
+  //   }
+  // }
 
 
   Future signInEmailPassword(LoginUser _login) async {
