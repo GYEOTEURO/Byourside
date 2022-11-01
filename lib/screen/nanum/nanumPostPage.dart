330d58d05efd626138498509f3a686688c9936dd
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-11-01 16:59:57 +0900

    Feat: 디버깅용 print문 추가 GradleException 에러 해결

--- lib/screen/nanum/nanumPostPage.dart
+++ lib/screen/nanum/nanumPostPage.dart
@@ -32,6 +32,7 @@ class _NanumPostPageState extends State<NanumPostPage> {
     if (images != null) {
       setState(() {
         _images = images; // 가져온 이미지를 _image에 저장
+        print(_images);
       });
     }
   }
