eae8838356369c0cc0815edcf6063c62935d6228
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-11-02 19:31:43 +0900

    Rename: post 디렉토리 ondo라는 이름으로 변경

--- lib/screen/nanum/nanumPostList.dart
+++ lib/screen/nanum/nanumPostList.dart
@@ -1,4 +1,4 @@
-import 'package:byourside/screen/post/postPage.dart';
+import 'package:byourside/screen/ondo/postPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
