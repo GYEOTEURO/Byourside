cae6c6fd5a40b60184427ef3e6816a9748a9429a
Shin-MG <alsruddl0407@kookmin.ac.kr>
2022-11-02 19:41:41 +0900

    Feat: Document CRUD 및 comment 기능 구현

--- lib/screen/bottomNavigationBar.dart
+++ lib/screen/bottomNavigationBar.dart
@@ -1,4 +1,4 @@
-import 'package:byourside/screen/postList.dart';
+import 'package:byourside/screen/ondo/postList.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatelessWidget {
