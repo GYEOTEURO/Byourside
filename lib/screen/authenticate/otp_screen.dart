2a92756ea7e77baf980488b204a027c5d5f8373a
HaEun <qlxqlrt2012@gmail.com>
2022-11-02 19:00:32 +0900

    Feat : update otp

--- lib/screen/authenticate/otp_screen.dart
+++ lib/screen/authenticate/otp_screen.dart
@@ -1,7 +1,9 @@
 import 'package:byourside/main.dart';
+import 'package:byourside/screen/authenticate/login.dart';
+import 'package:byourside/widget/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:byourside/screen/home/home.dart';
 import 'package:pinput/pinput.dart';
 
 class OTPScreen extends StatefulWidget {
@@ -13,16 +15,19 @@ class OTPScreen extends StatefulWidget {
 
 class _OTPScreenState extends State<OTPScreen> {
   final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
-  String? _verificationCode;
-  final TextEditingController _pinPutController = TextEditingController();
+  final TextEditingController otpCode = TextEditingController();
 
+  // final AuthService _auth = AuthService();
+
+  bool isLoading = false;
+  String? verificationId;
 
   final defaultPinTheme = PinTheme(
     width: 56,
     height: 56,
     textStyle: TextStyle(fontSize: 20, color: primaryColor, fontWeight: FontWeight.w600),
     decoration: BoxDecoration(
-      border: Border.all(color: Colors.black),
+      border: Border.all(color: primaryColor),
       borderRadius: BorderRadius.circular(20),
     ),
   );
@@ -50,19 +55,19 @@ class _OTPScreenState extends State<OTPScreen> {
               length: 6,
               defaultPinTheme: defaultPinTheme,
 
-              controller: _pinPutController,
+              controller: otpCode,
 
               pinAnimationType: PinAnimationType.fade,
               onSubmitted: (pin) async {
                 try {
                   await FirebaseAuth.instance
                       .signInWithCredential(PhoneAuthProvider.credential(
-                      verificationId: _verificationCode!, smsCode: pin))
+                      verificationId: verificationId!, smsCode: pin))
                       .then((value) async {
                     if (value.user != null) {
                       Navigator.pushAndRemoveUntil(
                           context,
-                          MaterialPageRoute(builder: (context) => Home()),
+                          MaterialPageRoute(builder: (context) => Login()),
                               (route) => false);
                     }
                   });
@@ -78,40 +83,61 @@ class _OTPScreenState extends State<OTPScreen> {
     );
   }
 
-  _verifyPhone() async {
+  verifyPhone() async {
     await FirebaseAuth.instance.verifyPhoneNumber(
         phoneNumber: '+82${widget.phone}',
-        verificationCompleted: (PhoneAuthCredential credential) async {
-          await FirebaseAuth.instance
-              .signInWithCredential(credential)
-              .then((value) async {
-            if (value.user != null) {
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => Home()),
-                      (route) => false);
-            }
-          });
-        },
+        verificationCompleted: _onVerificationCompleted,
         verificationFailed: (FirebaseAuthException e) {
           print(e.message);
         },
         codeSent: (String? verficationID, int? resendToken) {
           setState(() {
-            _verificationCode = verficationID;
+            this.verificationId = verficationID;
           });
         },
         codeAutoRetrievalTimeout: (String verificationID) {
           setState(() {
-            _verificationCode = verificationID;
+            this.verificationId = verificationID;
           });
         },
-        timeout: Duration(seconds: 120));
+        timeout: Duration(seconds: 60));
   }
+
+  _onVerificationCompleted(PhoneAuthCredential authCredential) async {
+    print("verification completed ${authCredential.smsCode}");
+    User? user = FirebaseAuth.instance.currentUser;
+    setState(() {
+      this.otpCode.text = authCredential.smsCode!;
+    });
+    if (authCredential.smsCode != null) {
+      try {
+        UserCredential credential =
+        await user!.linkWithCredential(authCredential);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'provider-already-linked') {
+          await FirebaseAuth.instance.signInWithCredential(authCredential);
+        }
+      }
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  // await FirebaseAuth.instance
+  //     .signInWithCredential(credential)
+  //     .then((value) async {
+  //         if (value.user != null) {
+  //             Navigator.pushAndRemoveUntil(
+  //                 context,
+  //                 MaterialPageRoute(builder: (context) => Home()),
+  //                 (route) => false);
+  //         }
+  // });
 
   @override
   void initState() {
     super.initState();
-    _verifyPhone();
+    verifyPhone();
   }
 }
