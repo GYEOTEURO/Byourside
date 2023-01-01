904aa2d8b1c2e462e6e4f5464658a14061844fd1
HaEun <qlxqlrt2012@gmail.com>
2023-01-01 15:04:30 +0900

    Style: just style changes

--- lib/screen/authenticate/wrapper.dart
+++ lib/screen/authenticate/wrapper.dart
@@ -5,26 +5,21 @@ import '../../model/firebase_user.dart';
 import 'handler.dart';
 import 'verify_email.dart';
 
-class Wrapper extends StatelessWidget{
-
+class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user =  Provider.of<FirebaseUser?>(context);
+    final user = Provider.of<FirebaseUser?>(context);
 
     print(user?.uid);
     print(user?.phoneNum);
     print(user?.displayName);
-    if(user == null)
-    {
+    if (user == null) {
       return Handler();
-    }
-    else if (user.phoneNum == null || user.phoneNum == "") {
+    } else if (user.phoneNum == null || user.phoneNum == "") {
       return Handler();
-    }
-    else if(user.displayName == null && user.phoneNum != null && user.phoneNum != "" || user.displayName == "") {
+    } else if (user.displayName == null || user.displayName == "") {
       return SetupUser();
-    }
-    else {
+    } else {
       return VerifyEmail();
     }
   }
