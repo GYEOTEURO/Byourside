dba71cb54164ddcaba670883f28a0b23826adc1f
YunHaaaa <qlxqlrt2012@gmail.com>
2023-07-04 17:20:35 +0900

    Feat: [BY-129] google login test code

---
+++ test/google_login_test.dart
@@ -0,0 +1,33 @@
+import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late MockGoogleSignIn googleSignIn;
+  setUp(() {
+    googleSignIn = MockGoogleSignIn();
+  });
+
+  test('should return idToken and accessToken when authenticating', () async {
+    var signInAccount = await googleSignIn.signIn();
+    var signInAuthentication = await signInAccount!.authentication;
+    expect(signInAuthentication, isNotNull);
+    expect(googleSignIn.currentUser, isNotNull);
+    expect(signInAuthentication.accessToken, isNotNull);
+    expect(signInAuthentication.idToken, isNotNull);
+  });
+
+  test('should return null when google login is cancelled by the user',
+      () async {
+    googleSignIn.setIsCancelled(true);
+    var signInAccount = await googleSignIn.signIn();
+    expect(signInAccount, isNull);
+  });
+  test('testing google login twice, once cancelled, once not cancelled at the same test.', () async {
+   googleSignIn.setIsCancelled(true);
+    var signInAccount = await googleSignIn.signIn();
+    expect(signInAccount, isNull);
+    googleSignIn.setIsCancelled(false);
+    var signInAccountSecondAttempt = await googleSignIn.signIn();
+    expect(signInAccountSecondAttempt, isNotNull);
+  });
+}
