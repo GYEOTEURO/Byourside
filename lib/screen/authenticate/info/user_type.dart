3818a21c2b24e78074829fbdef0adb87216ffc03
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-13 02:14:18 +0900

    Feat: [BY-211] Position buttons horizontally

--- lib/screen/authenticate/info/user_type.dart
+++ lib/screen/authenticate/info/user_type.dart
@@ -1,6 +1,6 @@
-import 'package:byourside/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:byourside/main.dart';
 
 class SetupUser extends StatefulWidget {
   const SetupUser({Key? key}) : super(key: key);
@@ -13,46 +13,46 @@ class _SetupUserState extends State<SetupUser> {
   String selectedOption = '';
 
   PreferredSizeWidget buildAppBar(BuildContext context) {
-  return AppBar(
-    centerTitle: true,
-    elevation: 0,
-    title: const Text(
-      '프로필을 완성해주세요',
-      semanticsLabel: '프로필을 완성해주세요',
-      style: TextStyle(
-        fontFamily: 'NanumGothic',
-        fontWeight: FontWeight.bold,
+    return AppBar(
+      centerTitle: true,
+      elevation: 0,
+      title: const Text(
+        '프로필을 완성해주세요',
+        semanticsLabel: '프로필을 완성해주세요',
+        style: TextStyle(
+          fontFamily: 'NanumGothic',
+          fontWeight: FontWeight.bold,
+        ),
       ),
-    ),
-    backgroundColor: primaryColor,
-  );
-}
-
+      backgroundColor: primaryColor,
+    );
+  }
 
   Widget buildButton(BuildContext context, String text) {
-    return Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(20.0),
-      color: Theme.of(context).primaryColor,
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width * 0.9,
-        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          HapticFeedback.lightImpact(); // 약한 진동
-          setState(() {
-            selectedOption = text;
-          });
-        },
-        child: Text(
-          text,
-          semanticsLabel: text,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontFamily: 'NanumGothic',
-            fontWeight: FontWeight.w500,
+    return Expanded(
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(20.0),
+        color: Theme.of(context).primaryColor,
+        child: MaterialButton(
+          padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {
+            HapticFeedback.lightImpact(); // 약한 진동
+            setState(() {
+              selectedOption = text;
+            });
+          },
+          child: Text(
+            text,
+            semanticsLabel: text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: 'NanumGothic',
+              fontWeight: FontWeight.w500,
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,
         ),
       ),
     );
@@ -65,32 +65,29 @@ class _SetupUserState extends State<SetupUser> {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(20.0),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Center(
-                      child: buildButton(context, '장애 아동 보호자'),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      buildButton(context, '장애 아동 보호자'),
+                      buildButton(context, '장애인'),
+                      buildButton(context, '종사자'),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Selected option: $selectedOption',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    buildButton(context, '장애인'),
-                    buildButton(context, '종사자'),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Selected option: $selectedOption',
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
