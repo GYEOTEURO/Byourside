b4641cd680d65f129abe09385f5dc1cbdc1f54d0
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-16 01:05:41 +0900

    [BY-210] Feat: user model

--- lib/model/auth.dart
+++ lib/model/auth.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:byourside/model/firebase_user.dart';
 
@@ -18,36 +19,78 @@ class AuthService {
   
   }
 
-  // Future signInCredential(LoginUser _login) async {
-  //   try {
-  //     await FirebaseAuth.instance
-  //         .signInWithCredential(PhoneAuthProvider.credential(
-  //         verificationId: _login.email!, smsCode: _login.password!))
-  //         .then((value) async {
-  //             return value;
-  //     });
-  //   } on FirebaseAuthException catch (e) {
-  //       return FirebaseUser(uid: null, code: e.code);
-  //   }
-  // }
-
-  Future signOut() async {
+    Future signOut() async {
     try {
       return await _auth.signOut();
     } catch (e) {
       return null;
     }
   }
-
-  // // Firebase로부터 회원 탈퇴
-  // Future withdrawalAccount(password) async {
-  //   try {
-  //     AuthCredential credential = await EmailAuthProvider.credential(
-  //         email: _auth.currentUser!.email!, password: password);
-  //     await _auth.currentUser!.reauthenticateWithCredential(credential);
-  //     return _auth.currentUser!.delete();
-  //   } catch (e) {
-  //     return null;
-  //   }
-  // }
 }
+// // https://kyungsnim.net/130
+// import 'package:byourside/model/login_user.dart';
+// import 'package:byourside/screen/authenticate/info/user_type.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:get/get.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+
+// final GoogleSignIn googleSignIn = GoogleSignIn();
+// final userReference = FirebaseFirestore.instance.collection('users');
+// final DateTime timestamp = DateTime.now();
+
+// User currentUser;
+
+// googleSignIn.onCurrentUserChanged.listen((gSignInAccount) {
+//   controlSignIn(gSignInAccount);
+// }, onError: (gError) {
+//   print('Error Message : ' + gError);
+// });
+
+// GoogleSignIn.signInSilently();
+
+// controlSignIn(GoogleSignInAccount signInAccount) async {
+//   if (signInAccount != null) {
+//     await saveUserInfoFirestore();
+//     setState(() {
+//       isSignedIn = true;
+//     });
+//   } else {
+//     setState(() {
+//       isSignedIn = false;
+//     });
+//   }
+// }
+
+// saveUserInfoToFirestore() async {
+//   var gCurrentUser = googleSignIn.currentUser;
+//   DocumentSnapshot documentSnapshot = await userReference.doc(gCurrentUser.id).get();
+
+//   if (!documentSnapshot.exists) {
+//     var username = Get.offAll(() => const SetupUser());
+
+//     userReference.doc(gCurrentUser.id).set({
+//       'id' : gCurrentUser.id,
+//       'profileName' : gCurrentUser.displayName,
+//       'userName' : username,
+//       'email' : gCurrentUser.email,
+//       'timestamp' : timestamp
+//     });
+    
+//     documentSnapshot = await userReference.doc(gCurrentUser.id).get();
+//   }
+
+//   currentUser = User.fromDocument(documentSnapshot);
+  
+// }
+
+// void dispose() {
+//   PageController.dispose();
+//   super.dispose();
+// }
+
+// loginUser() {
+//   googleSignIn.signOut();
+// }
+
