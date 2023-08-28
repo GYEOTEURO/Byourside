764c9b0cea46d6d0c4298814deaf2c64fc135412
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-29 02:43:54 +0900

    [BY-254] Feat: mypage logout function

--- lib/screen/mypage/my_page.dart
+++ lib/screen/mypage/my_page.dart
@@ -1,6 +1,4 @@
 import 'package:byourside/screen/mypage/options.dart';
-import 'package:byourside/screen/authenticate/controller/auth_controller.dart';
-import 'package:byourside/screen/authenticate/social_login/social_login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:byourside/constants/constants.dart' as constants;
@@ -15,21 +13,10 @@ class Mypage extends StatefulWidget {
 }
 
 class _Mypage extends State<Mypage> {
-  final AuthController _authController = AuthController.instance;
   late String uid;
   late String displayName;
   User? user;
-
-  void _logout(context) async {
-   _authController.logout();
-    setState(() {});
-    Navigator.of(context).popUntil((route) => route.isFirst);
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => const SocialLogin(
-      ),
-    ));
-    // Navigator.pushNamed(context, '/login');
-  }
+  
 
   @override
   Widget build(BuildContext context) {
--- lib/screen/mypage/options.dart
+++ lib/screen/mypage/options.dart
@@ -1,44 +1,83 @@
+import 'package:byourside/screen/authenticate/controller/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:byourside/constants/colors.dart' as colors;
 import 'package:byourside/constants/fonts.dart' as fonts;
 
+Future<bool> showLogoutConfirmationDialog(BuildContext context) async {
+  return await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('로그아웃 하시겠습니까?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false), // User didn't confirm
+            child: const Text('아니오'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true), // User confirmed
+            child: const Text('예'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<void> handleLogoutAction(BuildContext context) async {
+  AuthController authController = AuthController.instance;
+  bool confirmLogout = await showLogoutConfirmationDialog(context);
+
+  if (confirmLogout) {
+    authController.logout(); // 로그아웃 로직
+  }
+}
+
 Widget myPageOptions(BuildContext context, String optionName, List<Map<String, dynamic>> options) {
-    return Column(
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-          alignment: Alignment.centerLeft,
-          child: Text(
+  return Column(
+    children: [
+      Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        alignment: Alignment.centerLeft,
+        child: Text(
           optionName,
           style: const TextStyle(
-              color: colors.textColor,
-              fontSize: 15,
-              fontFamily: fonts.font,
-              fontWeight: FontWeight.w700,
-              height: 1.53,
-            ),
-          ), 
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.height / 15 * options.length,
-          decoration: ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                  side: const BorderSide(width: 0.50, color: colors.bgrColor),
-                  borderRadius: BorderRadius.circular(10),
-              ),
+            color: colors.textColor,
+            fontSize: 15,
+            fontFamily: fonts.font,
+            fontWeight: FontWeight.w700,
+            height: 1.53,
           ),
-          child: Column(
-            children: [
-              for(int i = 0; i < options.length; i++)
+        ),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height / 15 * options.length,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(width: 0.50, color: colors.bgrColor),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Column(
+          children: [
+            for (int i = 0; i < options.length; i++)
               OutlinedButton(
                 onPressed: () {
                   HapticFeedback.lightImpact(); // 약한 진동
-                  Navigator.push(
+                  if (options[i].containsKey('action')) {
+                    options[i]['action'](context);
+                  }
+                  if (options[i].containsKey('page')) {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => options[i]['page']));},
+                        builder: (context) => options[i]['page'],
+                      ),
+                    );
+                  }
+                },
                 style: OutlinedButton.styleFrom(
                   minimumSize: Size(MediaQuery.of(context).size.width * 0.8, MediaQuery.of(context).size.height / 16),
                   elevation: 0,
@@ -55,11 +94,13 @@ Widget myPageOptions(BuildContext context, String optionName, List<Map<String, d
                       fontWeight: FontWeight.w400,
                       height: 1.20,
                     ),
-                  )
-              )),
-              const Divider(color: colors.bgrColor, thickness: 1)
-          ])
-        )
-    ]);
+                  ),
+                ),
+              ),
+            const Divider(color: colors.bgrColor, thickness: 1),
+          ],
+        ),
+      ),
+    ],
+  );
 }
-
