44e241eaf5ec918f74c93d510ae66760fd3fa95a
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-28 19:05:44 +0900

    [BY-239] Feat: login screen design draft

--- lib/screen/authenticate/social_login/social_login.dart
+++ lib/screen/authenticate/social_login/social_login.dart
@@ -1,8 +1,8 @@
-
+import 'dart:io';
 import 'package:byourside/screen/authenticate/controller/auth_controller.dart';
 import 'package:byourside/widget/authenticate/social_login_button.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:byourside/constants/auth_icons.dart';
 
 
 class SocialLogin extends StatelessWidget {
@@ -10,6 +10,8 @@ class SocialLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isIOS = Platform.isIOS;
+
     return Scaffold(
       body: DecoratedBox(
         decoration: const BoxDecoration(
@@ -21,29 +23,138 @@ class SocialLogin extends StatelessWidget {
         ),
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SocialLoginButton(
-                buttonText: 'Google 로그인',
-                buttonColor: Colors.red,
-                iconColor: Colors.white,
-                icon: FontAwesomeIcons.google,
-                onPressed: (context) async {
-                  await AuthController.instance.loginWithGoogle(context);
+              _buildIconContainer(
+                width: 152.72,
+                height: 145.02,
+                svgIcon: AuthIcons.beesideLogo,
+              ),
+              const SizedBox(height: 100),
+              _buildButtonContainer(
+                color: Colors.white,
+                icon: _buildIconContainer(
+                  width: 39,
+                  height: 44,
+                  backgroundColor: Colors.white,
+                  svgIcon: AuthIcons.googleIcon,
+                ),
+                text: 'Google 계정으로 로그인',
+              ),
+              const SizedBox(height: 20),
+              if (isIOS) 
+              _buildButtonContainer(
+                color: Colors.white,
+                icon: _buildIconContainer(
+                  width: 39,
+                  height: 44,
+                  backgroundColor: Colors.white,
+                  svgIcon: AuthIcons.appleIcon,
+                ),
+                text: 'Apple로 로그인',
+              ),
+              const SizedBox(height: 30),
+              _buildTextButton(
+                text: '이용약관',
+                onPressed: () {
+                  // 버튼이 클릭되었을 때 실행될 동작을 여기에 추가
                 },
               ),
-              SocialLoginButton(
-                buttonText: 'Apple 로그인',
-                buttonColor: Colors.black,
-                iconColor: Colors.white,
-                icon: FontAwesomeIcons.apple,
-                onPressed: (context) async {
-                  await AuthController.instance.signInWithApple();
+              AuthIcons.tosLine,
+              const SizedBox(height: 5),
+              _buildTextButton(
+                text: '개인정보 처리방침',
+                onPressed: () {
+                  // 버튼이 클릭되었을 때 실행될 동작을 여기에 추가
                 },
               ),
+              AuthIcons.policyLine,
             ],
           ),
         ),
       ),
     );
   }
+
+
+Widget _buildButtonContainer({
+  required Color color,
+  required Widget icon,
+  required String text,
+}) {
+  return Container(
+    width: 328,
+    height: 43.96,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(22),
+      color: color,
+    ),
+    child: Stack(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: icon,
+            ),
+          ],
+        ),
+        Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 19,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+  Widget _buildIconContainer({
+    required double width,
+    required double height,
+    Color backgroundColor = Colors.transparent, 
+    required Widget svgIcon,
+  }) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+      ),
+      child: svgIcon,
+    );
+  }
+
+
+  Widget _buildTextButton({
+    required String text,
+    required VoidCallback onPressed,
+  }) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 13,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
 }
