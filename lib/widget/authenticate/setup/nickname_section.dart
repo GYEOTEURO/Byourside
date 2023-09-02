5b215508880f4a3e15a6f6e7dcff188d939fd3ce
YunHaaaa <qlxqlrt2012@gmail.com>
2023-09-02 10:24:31 +0900

    [BY-255] Design: nickname section design

--- lib/widget/authenticate/setup/nickname_section.dart
+++ lib/widget/authenticate/setup/nickname_section.dart
@@ -1,16 +1,35 @@
 import 'package:byourside/screen/authenticate/controller/nickname_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:byourside/constants/fonts.dart' as fonts;
+import 'package:byourside/constants/colors.dart' as colors;
 import 'package:get/get.dart';
 
-class NicknameSection extends StatelessWidget {
-  final NicknameController _nicknameController = Get.put(NicknameController());
-
+class NicknameSection extends StatefulWidget {
   NicknameSection({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Get.lazyPut<NicknameController>(() => NicknameController(), fenix: true);
+  _NicknameSectionState createState() => _NicknameSectionState();
+}
 
+class _NicknameSectionState extends State<NicknameSection> {
+  final NicknameController _nicknameController = Get.put(NicknameController());
+  double _deviceWidth = 0;
+  double _deviceHeight = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _deviceWidth = MediaQuery.of(context).size.width;
+        _deviceHeight = MediaQuery.of(context).size.height;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return buildNicknameContent(context);
   }
 
@@ -20,8 +39,16 @@ class NicknameSection extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          buildNicknameTextField(),
-          buildCheckNicknameButton(context),
+          Row(
+          children: [
+            Expanded(
+                child: buildNicknameTextField(),
+              ),
+              const SizedBox(width: 16), 
+              buildCheckNicknameButton(context),
+            ],
+          ),
+          const SizedBox(height: 10), 
           buildNicknameCheckResultText(),
         ],
       ),
@@ -34,24 +61,63 @@ class NicknameSection extends StatelessWidget {
       textField: true,
       label: '닉네임을 입력하세요.',
       hint: '(예: 홍길동)',
-      child: TextFormField(
-        controller: _nicknameController.controller,
-        decoration: const InputDecoration(
-          labelText: '닉네임을 입력하세요',
-          hintText: '(예: 홍길동) ',
+      child: SizedBox(
+        width: _deviceWidth * 0.66,
+        height: _deviceHeight * 0.07,
+        child: TextFormField(
+          onTap: () {
+            HapticFeedback.lightImpact();
+          },
+          controller: _nicknameController.controller,
+          maxLines: 1,
+          decoration: InputDecoration(
+            labelText: '닉네임을 입력하세요',
+            fillColor: colors.bgrColor,
+            filled: true,
+            hintText: '(예: 홍길동) ',
+            labelStyle: const TextStyle(
+              color: colors.textColor,
+              fontSize: 12,
+              fontFamily: fonts.font,
+              fontWeight: FontWeight.w500,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(28),
+              borderSide: BorderSide.none,
+            ),
+          ),
+          autofocus: true,
         ),
-        autofocus: true,
       ),
     );
   }
 
   Widget buildCheckNicknameButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
+    return GestureDetector(
+      onTap: () async {
         await _nicknameController.checkNicknameExist(context);
         _nicknameController.isNicknameChecked.value = true;
       },
-      child: const Text('중복확인'),
+      child: Container(
+        width: _deviceWidth * 0.26,
+        height: _deviceHeight * 0.07,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(69),
+          color: const Color(0xffffc700),
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '중복확인',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700, // 오타 수정: FontWeith -> FontWeight
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -65,7 +131,8 @@ class NicknameSection extends StatelessWidget {
           style: TextStyle(
             color: _nicknameController.nickNameExist.value ? Colors.red : Colors.green,
             fontFamily: 'NanumGothic',
-            fontWeight: FontWeight.w500,
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
           ),
         ));
   }
