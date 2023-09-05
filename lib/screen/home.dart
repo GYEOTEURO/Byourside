28a57241e9d78731455433af8560b8487b4c55b0
anjiwon319 <anjiwon319@kookmin.ac.kr>
2023-09-06 06:24:05 +0900

    [BY-257] Feat: 홈화면에서 정보게시판 인기글 타일 연결

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,6 +5,7 @@ import 'package:byourside/screen/authenticate/controller/user_controller.dart';
 import 'package:byourside/model/community_post.dart';
 import 'package:byourside/screen/community/community_post_list_tile.dart';
 import 'package:byourside/widget/app_bar_select_button.dart';
+import 'package:byourside/widget/auto_information/stream_autoInfo_post.dart';
 import 'package:byourside/widget/icon_buttons.dart';
 import 'package:byourside/widget/no_data.dart';
 import 'package:byourside/widget/stream_community_post.dart';
@@ -16,9 +17,7 @@ import 'package:get/get.dart';
 import '../../model/load_data.dart';
 
 class Home extends StatefulWidget {
-  const Home(
-      {Key? key})
-      : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -27,103 +26,89 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final User? user = FirebaseAuth.instance.currentUser;
   final LoadData loadData = LoadData();
-  String? disabilityType = Get.find<UserController>().userModel.disabilityType!.split(' ')[0];
+  String? disabilityType =
+      Get.find<UserController>().userModel.disabilityType!.split(' ')[0];
   String? district = Get.find<UserController>().userModel.district;
+  String? area = Get.find<UserController>().userModel.area;
 
-  Widget _appbar(){
+  Widget _appbar() {
     return Container(
-              height: MediaQuery.of(context).size.height / 6.5,
-              color: colors.homeAppBarColor,
-              child: Column(
-                children: [
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Row(
-                          children: [
-                            const SizedBox(width: 10),
-                            appBarSelectButton(context, district!),
-                            const SizedBox(width: 5),
-                            appBarSelectButton(context, disabilityType!)
-                          ])),
-                      Row(
-                        children: [
-                          goToScrapPage(context),
-                          goToSearchPage(context)
-                        ])
-                    ]),
-                    Center(child: customIcons.logo),
-              ])
-            );
-  } 
+        height: MediaQuery.of(context).size.height / 6.5,
+        color: colors.homeAppBarColor,
+        child: Column(children: [
+          const SizedBox(height: 15),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Expanded(
+                child: Row(children: [
+              const SizedBox(width: 10),
+              appBarSelectButton(context, district!),
+              const SizedBox(width: 5),
+              appBarSelectButton(context, disabilityType!)
+            ])),
+            Row(children: [goToScrapPage(context), goToSearchPage(context)])
+          ]),
+          Center(child: customIcons.logo),
+        ]));
+  }
 
-  Widget _titleSeeMore(SvgPicture icon, String title, int index){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        icon,
-        Text(
-          title,
-          style: const TextStyle(
-              color: colors.textColor,
-              fontSize: 20,
-              fontFamily: fonts.font,
-              fontWeight: FontWeight.w700,
-              height: 1.50,
-          ),
-        )
+  Widget _titleSeeMore(SvgPicture icon, String title, int index) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      icon,
+      Text(
+        title,
+        style: const TextStyle(
+          color: colors.textColor,
+          fontSize: 20,
+          fontFamily: fonts.font,
+          fontWeight: FontWeight.w700,
+          height: 1.50,
+        ),
+      )
     ]);
   }
 
-  Widget _bubbleHobee(){
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              customIcons.speechBubble,
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 6),
-                child: Text(
-                '$district민들의 이야기를 들어보세요',
-                style: const TextStyle(
-                    color: colors.textColor,
-                    fontSize: 13,
-                    fontFamily: fonts.font,
-                    fontWeight: FontWeight.w400
-                ),
+  Widget _bubbleHobee() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Stack(alignment: Alignment.center, children: [
+        customIcons.speechBubble,
+        Container(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 6),
+            child: Text(
+              '$district민들의 이야기를 들어보세요',
+              style: const TextStyle(
+                  color: colors.textColor,
+                  fontSize: 13,
+                  fontFamily: fonts.font,
+                  fontWeight: FontWeight.w400),
             ))
-            ]
-          ),
-          customIcons.hobee
-      ]);
+      ]),
+      customIcons.hobee
+    ]);
   }
-  Widget _autoInformationNewPosts(){
-    return Container(
-      height: MediaQuery.of(context).size.height / 5,
-    );
-  }
-  Widget _communityHotPosts(){
+
+  Widget _autoInformationNewPosts() {
     return Flexible(
-      child: streamCommunityPost(() => loadData.readHotCommunityPosts(disabilityType: disabilityType))
+      child: streamAutoInfoPost(() => loadData.readNewAutoInformationPosts(
+          disabilityType: disabilityType, area: area, district: district)),
     );
+  }
+
+  Widget _communityHotPosts() {
+    return Flexible(
+        child: streamCommunityPost(() =>
+            loadData.readHotCommunityPosts(disabilityType: disabilityType)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          _appbar(),
-          _titleSeeMore(customIcons.autoInformation, '정보 게시판 최신글', 0),
-          _autoInformationNewPosts(),
-          _bubbleHobee(),
-          _titleSeeMore(customIcons.community, '소통 게시판 인기글', 1),
-          _communityHotPosts()
-        ])
-      ); 
+        body: Column(children: [
+      _appbar(),
+      _titleSeeMore(customIcons.autoInformation, '정보 게시판 최신글', 0),
+      _autoInformationNewPosts(),
+      _bubbleHobee(),
+      _titleSeeMore(customIcons.community, '소통 게시판 인기글', 1),
+      _communityHotPosts()
+    ]));
   }
 }
---
+++ lib/widget/auto_information/stream_autoInfo_post.dart
@@ -0,0 +1,22 @@
+import 'package:byourside/model/autoInformation_post.dart';
+import 'package:byourside/screen/autoInformation/autoInfo_post_list_tile.dart';
+import 'package:byourside/widget/no_data.dart';
+import 'package:flutter/material.dart';
+
+Widget streamAutoInfoPost(Function loadData) {
+  return StreamBuilder<List<AutoInformationPostModel>>(
+      stream: loadData(),
+      builder: (context, snapshots) {
+        if (snapshots.hasData) {
+          return ListView.builder(
+              itemCount: snapshots.data!.length,
+              shrinkWrap: true,
+              itemBuilder: (_, index) {
+                AutoInformationPostModel post = snapshots.data![index];
+                return autoInfoPostListTile(context, post);
+              });
+        } else {
+          return noData();
+        }
+      });
+}
