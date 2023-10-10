b69b0e5192a309a1dfe7729618fafeb82f1fbb1d
Shin-MG <alsruddl0407@kookmin.ac.kr>
2023-10-10 19:35:31 +0900

    [BY-325] Refactor: constants.dart 파일 세분화해 정리

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,14 +3,6 @@ constants.dart : 앱에서 사용하는 각종 상수 값을 정리한 파일
 - 상수명 lowerCamelCase 스타일 따름 (Dart 권장 : PREFER using lowerCamelCase for constant names)
 */
 
-import 'package:byourside/constants/text.dart';
-import 'package:byourside/screen/authenticate/policy.dart';
-import 'package:byourside/screen/mypage/my_block_user.dart';
-import 'package:byourside/screen/mypage/my_community_post.dart';
-import 'package:byourside/screen/common/my_scrap.dart';
-import 'package:byourside/screen/mypage/options.dart';
-import 'package:byourside/screen/mypage/setting.dart';
-
 // 버튼
 double buttonBorderWidth = 0.50;
 
@@ -40,30 +32,3 @@ List<String> autoInformationCategories = [
   '지원금',
 ];
 
-
-List<Map<String, dynamic>> myActivity = [
-  {'name': '스크랩한 게시물', 'page': const MyScrap()},
-  {'name': '내가 쓴 게시물', 'page': const MyCommunityPost()}
-];
-
-List<Map<String, dynamic>> etc = [
-  {'name': '설정', 'page': Setting(options: setting)},
-  // {
-  //   'name' : '개발자에게 문의하기',
-  //   'page' : ToDeveloper()
-  // },
-  {
-    'name': '로그아웃',
-    'action': handleLogoutAction,
-  }
-];
-
-List<Map<String, dynamic>> setting = [
-  {'name': '차단목록', 'page': const MyBlock()},
-  {'name': '개인정보처리방침', 'page': Policy(policy: personalData)},
-  {'name': '서비스 이용약관', 'page': Policy(policy: usingPolicy)},
-  {
-    'name': '탈퇴하기',
-    'action': handleDeleteAction,
-  }
-];
