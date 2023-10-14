e94934a72f641534dce4a1ed1665f8a3c505b8ad
YunHaaaa <qlxqlrt2012@gmail.com>
2023-10-15 01:55:54 +0900

    [BY-351] Refactor: institution name field w, h

--- lib/widget/authenticate/setup/institution_name_field.dart
+++ lib/widget/authenticate/setup/institution_name_field.dart
@@ -14,8 +14,8 @@ class InstitutionNameTextField extends StatefulWidget {
 }
 
 class InstitutionNameTextFieldState extends State<InstitutionNameTextField> {
-  double deviceWidth = 0;
-  double deviceHeight = 0;
+  double _deviceWidth = 0;
+  double _deviceHeight = 0;
 
   @override
   void initState() {
@@ -23,17 +23,25 @@ class InstitutionNameTextFieldState extends State<InstitutionNameTextField> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
         var mediaQuery = MediaQuery.of(context);
-        deviceWidth = mediaQuery.size.width;
-        deviceHeight = mediaQuery.size.height;
+        _deviceWidth = mediaQuery.size.width;
+        _deviceHeight = mediaQuery.size.height;
       });
     });
   }
 
+
+  double getRelativeWidth(double value) {
+    return _deviceWidth * value;
+  }
+
+  double getRelativeHeight(double value) {
+    return _deviceHeight * value;
+  }
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: deviceWidth * 0.66,
-      height: deviceHeight * 0.07,
+      width: getRelativeWidth(0.66),
+      height: getRelativeHeight(0.07),
       child: TextFormField(
         onTap: widget.onTap,
         controller: widget.controller,
@@ -45,12 +53,12 @@ class InstitutionNameTextFieldState extends State<InstitutionNameTextField> {
           hintText: text.institutionNameHint,
           labelStyle: TextStyle(
             color: colors.textColor,
-            fontSize: deviceWidth * 0.036,
+            fontSize: getRelativeWidth(0.036),
             fontFamily: fonts.font,
             fontWeight: FontWeight.w500,
           ),
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(deviceWidth * 0.087),
+            borderRadius: BorderRadius.circular(getRelativeWidth(0.087)),
             borderSide: BorderSide.none,
           ),
         ),
