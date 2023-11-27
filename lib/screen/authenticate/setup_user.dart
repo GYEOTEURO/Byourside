a33d721a87386acf6f613ae0dc7f6499983355bf
YunHaaaa <qlxqlrt2012@gmail.com>
2023-11-27 18:23:35 +0900

    [BY-444] Refactor: change function name

--- lib/screen/authenticate/setup_user.dart
+++ lib/screen/authenticate/setup_user.dart
@@ -1,9 +1,9 @@
-import 'package:byourside/widget/authenticate/setup/institution_name_field.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:byourside/constants/text.dart' as text;
+import 'package:byourside/constants/colors.dart' as colors;
 import 'package:byourside/utils/validation_utils.dart';
 import 'package:byourside/widget/complete_button.dart';
 import 'package:byourside/widget/common/title_only_appbar.dart';
@@ -13,10 +13,11 @@ import 'package:byourside/widget/authenticate/setup/age_section.dart';
 import 'package:byourside/widget/authenticate/setup/purpose_select.dart';
 import 'package:byourside/widget/authenticate/setup/nickname_section.dart';
 import 'package:byourside/screen/authenticate/controller/auth_controller.dart';
+import 'package:byourside/widget/authenticate/setup/institution_name_field.dart';
 import 'package:byourside/widget/authenticate/setup/disability_type_button.dart';
 import 'package:byourside/screen/authenticate/controller/nickname_controller.dart';
 import 'package:byourside/widget/authenticate/setup/user_type/user_type_selection.dart';
-import 'package:byourside/constants/colors.dart' as colors;
+
 
 class SetupUser extends StatefulWidget {
   const SetupUser({Key? key}) : super(key: key);
@@ -25,20 +26,24 @@ class SetupUser extends StatefulWidget {
   State<SetupUser> createState() => _SetupUserState();
 }
 
+
 class _SetupUserState extends State<SetupUser> {
-  final TextEditingController _birthYear = TextEditingController();
-  final NicknameController _nicknameController = Get.find<NicknameController>();
-  final TextEditingController _institutionNameController = TextEditingController();
+
+  final TextEditingController birthYearController = TextEditingController();
+  final TextEditingController institutionNameController = TextEditingController();
+  final NicknameController nicknameController = Get.find<NicknameController>();
+
   Map<String, String>? location;
-  String _selectedUserType = '';
-  String _selectedDisabilityType = '';
-  String _selectedPurpose = '';
+
+  String selectedUserType = '';
+  String selectedDisabilityType = '';
+  String selectedPurpose = '';
 
   final User? user = FirebaseAuth.instance.currentUser;
 
-  void _handleUserTypeSelected(String userType) {
+  void handleUserTypeSelected(String userType) {
     setState(() {
-      _selectedUserType = userType;
+      selectedUserType = userType;
     });
   }
 
@@ -48,53 +53,51 @@ class _SetupUserState extends State<SetupUser> {
     });
   }
 
-
-  void _handleLocationSelected(String selectedArea, String selectedDistrict) {
+  void handleLocationSelected(String selectedArea, String selectedDistrict) {
     Map<String, String> locationInfo = {
       'area': selectedArea,
       'district': selectedDistrict,
     };
 
-    onLocationChanged(locationInfo); 
+    onLocationChanged(locationInfo);
   }
 
-  void _handleDisabilityTypeSelected(String type) {
+  void handleDisabilityTypeSelected(String type) {
     setState(() {
-      _selectedDisabilityType = type;
+      selectedDisabilityType = type;
     });
   }
 
-  void _onCompleteButtonPressed(BuildContext context) async {
+  void onCompleteButtonPressed(BuildContext context) async {
     HapticFeedback.lightImpact();
-    var age = int.tryParse(_birthYear.text) ?? 0;
-    var inputsValid = _validateInputs();
+    var age = int.tryParse(birthYearController.text) ?? 0;
+    var inputsValid = validateInputs();
 
     if (inputsValid) {
       SaveUserData.saveUserInfo(
         birthYear: age,
-        disabilityType: _selectedDisabilityType,
-        institutionName: _institutionNameController.text,
+        disabilityType: selectedDisabilityType,
+        institutionName: institutionNameController.text,
         location: location,
-        nickname: _nicknameController.controller.text,
-        registrationPurpose: _selectedPurpose,
-        userType: _selectedUserType,
+        nickname: nicknameController.controller.text,
+        registrationPurpose: selectedPurpose,
+        userType: selectedUserType,
       );
 
       AuthController.instance.handleUserInfoCompletion();
     }
   }
 
-  bool _validateInputs() {
-    
+  bool validateInputs() {
     return ValidationUtils.validateInputs(
       context,
-      _nicknameController,
-      _selectedUserType,
-      _selectedDisabilityType,
-      _institutionNameController.text,
-      _birthYear.text,
+      nicknameController,
+      selectedUserType,
+      selectedDisabilityType,
+      institutionNameController.text,
+      birthYearController.text,
       location,
-      _selectedPurpose,
+      selectedPurpose,
     );
   }
 
@@ -116,18 +119,18 @@ class _SetupUserState extends State<SetupUser> {
                   children: [
                     const NicknameSection(),
                     UserTypeSelection(
-                      selectedType: _selectedUserType,
-                      onTypeSelected: _handleUserTypeSelected,
+                      selectedType: selectedUserType,
+                      onTypeSelected: handleUserTypeSelected,
                     ),
                     DisabilityType(
-                      initialType: _selectedDisabilityType,
-                      onChanged: _handleDisabilityTypeSelected,
+                      initialType: selectedDisabilityType,
+                      onChanged: handleDisabilityTypeSelected,
                     ),
-                    (_selectedUserType == text.worker)
+                    (selectedUserType == text.worker)
                         ? Align(
                             alignment: Alignment.centerLeft, 
                             child: InstitutionNameTextField(
-                              controller: _institutionNameController,
+                              controller: institutionNameController,
                               onTap: () {
                                 HapticFeedback.lightImpact();
                               },
@@ -135,14 +138,14 @@ class _SetupUserState extends State<SetupUser> {
                           )
                         : const SizedBox.shrink(),
                     AgeSection(
-                      selectedType: _selectedUserType,
-                      controller: _birthYear,
+                      selectedType: selectedUserType,
+                      controller: birthYearController,
                     ),
-                    LocationSection(onLocationSelected: _handleLocationSelected),
+                    LocationSection(onLocationSelected: handleLocationSelected),
                     AppPurposeSelection(
                       onChanged: (purpose) {
                         setState(() {
-                          _selectedPurpose = purpose;
+                          selectedPurpose = purpose;
                         });
                       },
                     ),
@@ -151,7 +154,7 @@ class _SetupUserState extends State<SetupUser> {
               ),
             ),
             CompleteButton(
-              onPressed: () => _onCompleteButtonPressed(context),
+              onPressed: () => onCompleteButtonPressed(context),
               text: text.start,
             ),
           ],
